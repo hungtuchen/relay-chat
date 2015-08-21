c29fe09abdf0213422df2551bd56303b6013eeca
transedward <transedward@gmail.com>
2015-08-21 19:38:16 +0800

    add more explanation and refactor

--- js/components/ChatApp.js
+++ js/components/ChatApp.js
@@ -23,18 +23,27 @@ class ChatApp extends React.Component {
 
   componentWillMount() {
     // by default, set threads[0].id to currentID if pathname === '/'
+    // 如果 route 是'/'的時候自動把 route 轉到 threads[0] 的 id 對應到的
+    // 'thread/:id' 去，因為目前無法在更上層 access 到 這個 id, 所以先這樣做
     // TODO: better if we can do it in route config
-    let currentThreadID = this.props.viewer.threads.edges[0].node.id;
+    const currentThreadID = this.props.viewer.threads.edges[0].node.id;
     if (window.location.pathname === '/' ) {
       this.context.router.transitionTo(`/thread/${currentThreadID}`);
     }
   }
 
   render() {
-    let {viewer} = this.props;
+    // the specified fragments below would be this.props here
+    // Note: Relay ask you to pass props(ex: thread) to child component
+    // if it need to use this fragement(thread), think it as corresponding
+    // ThreadSection.getFragment... belowing
+    // 下面 specify 的 fragments 會變成這裡的 this.props
+    // 注意： Relay 要求你在下層(ex: ThreadSection)用到的 fragments
+    // 這裡要當 props 傳下去, 可以想成要跟 ThreadSection.getFragment...對應到
+    const {viewer, viewer: {threads}} = this.props;
     return (
       <div className="chatapp">
-        <ThreadSection threads={viewer.threads} viewer={viewer}/>
+        <ThreadSection threads={threads} viewer={viewer}/>
         {this.props.children}
       </div>
     );
@@ -44,6 +53,10 @@ class ChatApp extends React.Component {
 
 export default Relay.createContainer(ChatApp, {
   fragments: {
+    // Note: Relay will ask you to specify first, last, before, or after
+    // if you need to query to edges, so we set a big number here
+    // 注意：Relay 會要你 specify specify first, last, before, 或 after
+    // 如果你要 query 任何 edges 裡的東西, 所以我們這裡假定一個很大的數
     viewer: () => Relay.QL`
       fragment on User {
         threads(first: 9007199254740991) {
--- js/mutations/MarkThreadAsReadMutation.js
+++ js/mutations/MarkThreadAsReadMutation.js
@@ -1,6 +1,12 @@
 import Relay from 'react-relay';
 
 export default class MarkThreadAsReadMutation extends Relay.Mutation {
+  // fragments here specify thread, viewer then become this.props in
+  // belowing functions, from this point, mutation kind of like
+  // a component without rendering
+  // 這裡 specify 的 thread, viewer 就成為底下的 functions 可用的 props
+  // 從這個角度來看 mutation 其實有點像不會 render 的 component
+
   static fragments = {
     thread: () => Relay.QL`
       fragment on Thread {
@@ -17,9 +23,21 @@ export default class MarkThreadAsReadMutation extends Relay.Mutation {
       }
     `,
   };
+
+  // here you decide the mutation you wanna use in schema.js
+  // you can use different mutation base on different props...
+  // 這裡決定你要用什麼在 schema.js 定義過的 mutation
+  // 你可以根據不同的 props 用不同的 mutation
   getMutation() {
     return Relay.QL`mutation{markThreadAsRead}`;
   }
+
+  // What's special about FatQuery is that you can specify everything would
+  // be changeed based on this muatation, fragment on ... corresponding to
+  // the output of the mutation in schema.js
+  // FatQuery 很特別的是你可以 specify 所有會因此改變的其他東西,
+  // fragment on 後面對應到的是 schema.js 裡 mutation 的 output
+  // 可以想成根據 output 來更動所有頁面會因此變動的 field
   getFatQuery() {
     return Relay.QL`
       fragment on MarkThreadAsReadPayload {
@@ -34,6 +52,16 @@ export default class MarkThreadAsReadMutation extends Relay.Mutation {
       }
     `;
   }
+
+  // getConfigs tell Relay how to handle the payload of this muatation
+  // here we specify thread, viewer id to tell Relay there is some change
+  // for this two field (thread: isRead, viewer: unreadCount)
+  // There are four different types: FIELDS_CHANGE, NODE_DELETE,
+  // RANDE_ADD, RANDE_DELETE
+  // getConfigs 是告訴 Relay 要以什麼方便來處理這次 mutation 的 payload
+  // 這裡我們 specify thread, viewer 的 id 告訴 Relay thread, viewer 這兩個
+  // field的狀態都有改變了 (thread: isRead, viewer: unreadCount)
+  // 總共有 FIELDS_CHANGE, NODE_DELETE, RANDE_ADD, RANDE_DELETE 四種 types
   getConfigs() {
     return [{
       type: 'FIELDS_CHANGE',
@@ -43,11 +71,22 @@ export default class MarkThreadAsReadMutation extends Relay.Mutation {
       },
     }];
   }
+
+  // variables here is for the input of this mutation in schema.js
+  // don't confuse with variables in fragments
+  // 這裡的 variables 是 schema.js 裡 mutation 的 input ，別跟 fragments 搞混了
   getVariables() {
     return {
       id: this.props.thread.id,
     };
   }
+
+  // OptimisticResponse is how you wanna do optimistic updates,
+  // notice how similar is the shape returned here with FatQuery
+  // and you don't need specify every field like FatQuery
+  // OptimisticResponse 顧名思義就是你要怎麼作 optimistic updates
+  // 注意到這裡 return 的東西跟 FatQuery 的相似性，都是針對 output 要怎麼更新
+  // 不過不需要完全跟 FatQuery 一樣， 你可以不會 updates 全部
   getOptimisticResponse() {
     let viewerPayload;
     const {id, threads} = this.props.viewer;
